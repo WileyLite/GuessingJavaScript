b75fe66d58a80027f3b7d0af685f5da89fbb24f5
Jalin Wiley <jmt.wiley@gmail.com>
2020-01-14 23:35:29 -0500

    Pseudo coded functionality of game

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+// When user makes a guess: 
+// if user guesses th eright letter add 1 to wins tag, alert a win response
+//if wrong take away 1 guess
+//if guessleft=0 add 1 to losses  alert a loss to user and reset game
+   //reset guesses left as well
+//if letter guessed prev alert user that letter was already guessed
+//if character guessed that is not a letter alert user to guess a letter
+//create onekeyup function that loops through letter array
+//create function that tells computer to start game and pick a letter
+//create function that appends wins and losses and letter guesses
